96c821063c38b672c1f1903cf3b435c9ad0c01eb
Ruslan Himadieiev <gimadejev86@gmail.com>
2023-05-11 01:15:37 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,55 +1,71 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import { useState } from 'react';
 import css from './ContactForm.module.css';
 
-class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: ''
-  }
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumder] = useState('');
 
-  handleInputChange = e => {
+  const handleInputChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  }
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumder(value);
+        break;
+      default:
+        return;
+    }
+  };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
-  }
+    onSubmit({ name, number });
+    resetForm();
+  };
 
-  resetForm = () => {
-    this.setState({
-      name: '',
-      number: ''
-    })
-  }
+  const resetForm = () => {
+    setName('');
+    setNumder('');
+  };
 
-  render() {
-    return (
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <label className={css.label}>
-            Name<br/>
-          <input className={css.input} type="text" value={this.state.name}
-            onChange={this.handleInputChange} name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          required />
-          </label>
-          <label className={css.label}>
-            <br/>Number<br/>
-          <input className={css.input} type="tel" value={this.state.number}
-            onChange={this.handleInputChange} name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          required/>
-          </label>
-          <button className={css.button} type="submit">Add contact</button>
-        </form>
-    );
-  }  
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
+        <br />
+        <input
+          className={css.input}
+          type="text"
+          value={name}
+          onChange={handleInputChange}
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          required
+        />
+      </label>
+      <label className={css.label}>
+        <br />
+        Number
+        <br />
+        <input
+          className={css.input}
+          type="tel"
+          value={number}
+          onChange={handleInputChange}
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          required
+        />
+      </label>
+      <button className={css.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
-
-export default ContactForm;
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
